f645af47d6ffa0e0d5b285273c410b4b67aaa5a8
sarangkulkarniamb <sarangkulkarni1104@gmail.com>
2024-12-19 20:24:54 +0530

    skeleton complete

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+// all imports
 const express = require('express')
+const usersRouter=require("./routes/users")
+const coursesRouter=require("./routes/courses")
+const adminRouter=require("./routes/admin")
 const app = express()
 
 // Imp Middlewares
@@ -6,12 +10,9 @@ app.use(express.json())
 
 
 //Route Handler
-const usersRouter=require("./routes/users")
 app.use("/users",usersRouter)
-
-const coursesRouter=require("./routes/courses")
 app.use("/courses",coursesRouter)
-
+app.use("/admin",adminRouter)
 
 //Home Route
 app.get('/', (req, res) => res.send('Hello World!'))
